5d7f7eefa6997feb44607e8e67d2a8ba2839cd14
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2025-07-04 02:19:56 +0200

    Hong's changes; sumw drops all LHEReweightingSumw factors when available for aQGC

--- src/qawa/process/coffea_sumw.py
+++ src/qawa/process/coffea_sumw.py
@@ -14,10 +14,8 @@ class coffea_sumw(processor.ProcessorABC):
         if is_data:
             sumw = -1.0
         else:
-            if 'LHEReweightingSumw' in event.fields and 'aQGC' in dataset_name:
-                sumw = ak.sum(event.LHEReweightingSumw[:, 0])
-            else:
-                sumw = ak.sum(event.genEventSumw)
+            sumw = ak.sum(event.genEventSumw)
+
         return {dataset_name: sumw}
 
     def postprocess(self, accumulator):
