76713f35a728bdd124f1b354c00b96a99f6d89a3
Nick <38217274+NJManganelli@users.noreply.github.com>
2026-02-17 07:40:29 -0600

    Apply suggestion from @NJManganelli

--- src/qawa/tauSF.py
+++ src/qawa/tauSF.py
@@ -67,7 +67,7 @@ class tauIDScaleFactors:
 
         # {self.tagger}VSjet
         # sf_vsjet = self.corr_vsjet.evaluate(tau_pt,tau_dm,tau_genmatch, self.vsjet_wp, self.vse_wp, syst,"pt")
-        sf_vsjet = self.corr_vsjet.evaluate(tau_pt,tau_dm,tau_genmatch, self.vsjet_wp, "VVLoose", syst,"pt") 
+        sf_vsjet = self.corr_vsjet.evaluate(tau_pt,tau_dm,tau_genmatch, self.vsjet_wp, "VVLoose", syst,"pt") # vsjet evaluation fixed to "VVLoose" vsele ID
         sf_vsjet = ak.fill_none(sf_vsjet, 1.0)
         sf_vsjet = ak.unflatten(sf_vsjet, ntaus)
         sf_vsjet = ak.prod(sf_vsjet, axis=-1)
